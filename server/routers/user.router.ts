4d64a7ddcbaeecab8d0000068b0a7cf9b97741e4
Kevin Trần <tintt.pk@gmail.com>
2023-11-12 06:05:02 +0700

    Add function reshresh token

--- server/routers/user.router.ts
+++ server/routers/user.router.ts
@@ -1,5 +1,12 @@
 import express from "express";
-import {activateUser, authorizeRoles, loginUser, logoutUser, registrationUser} from "../controllers/user.controller";
+import {
+    activateUser,
+    authorizeRoles,
+    loginUser,
+    logoutUser,
+    registrationUser,
+    updateAccessToken
+} from "../controllers/user.controller";
 import {isAuthenticated} from "../middleware/auth";
 const userRouter = express.Router();
 
@@ -7,4 +14,5 @@ userRouter.post('/registration', registrationUser);
 userRouter.post('/activate-user', activateUser);
 userRouter.post('/login-user', loginUser);
 userRouter.post('/logout-user', isAuthenticated, logoutUser);
+userRouter.post('/refresh', updateAccessToken);
 export default userRouter;
